d6cce70bb3055049f1b72a0c8e6b3e8a00365eb4
Ville Saarivaara <ville.saarivaara@gmail.com>
2025-10-30 11:10:46 +0200

    Remove unused tests

--- tests/Feature/PageParserTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// $basicPageContainer = \ProcessWire\wire()->pages->get(1017);
-
-// test('query endpoint of service without base path', function () use (
-//   $basicPageContainer,
-// ) {
-//   var_dump($basicPageContainer->title);
-// });
