8aae40be21a59259dcc984f46f978fd334c27888
Ville Saarivaara <ville.saarivaara@gmail.com>
2026-03-07 15:06:25 +0200

    Add tests for Service::_prepare()

--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -6,3 +6,21 @@ test('name equals to class name', function () {
   $service = new FoodService();
   expect($service->name)->toBe('FoodService');
 });
+
+test('_prepare() calls init()', function () {
+  $service = new FoodService();
+  $service->_prepare();
+  expect($service->getEndpoints())->not->toBeEmpty();
+});
+
+test('_prepare() only calls init() once', function () {
+  $service = new FoodService();
+  $service->_prepare();
+  $service->_prepare();
+  expect($service->getEndpoints())->toHaveCount(1);
+});
+
+test('_prepare() returns the service instance', function () {
+  $service = new FoodService();
+  expect($service->_prepare())->toBe($service);
+});
