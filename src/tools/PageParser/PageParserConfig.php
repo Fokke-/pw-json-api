3901ae28c225188e1df45a8772f8a059b6a6b989
Ville Saarivaara <ville.saarivaara@gmail.com>
2026-03-07 18:35:12 +0200

    Mention page parser config defaults

--- src/tools/PageParser/PageParserConfig.php
+++ src/tools/PageParser/PageParserConfig.php
@@ -7,27 +7,27 @@ namespace PwJsonApi;
  */
 class PageParserConfig
 {
-  /** Recursively parse child pages? */
+  /** Recursively parse child pages? (default: false) */
   public bool $parseChildren = false;
 
-  /** Selector for child pages */
+  /** Selector for child pages (default: '') */
   public string $childrenSelector = '';
 
-  /** Key name for child pages */
+  /** Key name for child pages (default: '_children') */
   public string $childrenKey = '_children';
 
-  /** Recursively parse children of page field references? */
+  /** Recursively parse children of page field references? (default: false) */
   public bool $parsePageReferenceChildren = false;
 
-  /** Maximum depth for recursive parsing */
+  /** Maximum depth for recursive parsing (default: 3) */
   public int $maxDepth = 3;
 
-  /** Output full file URLs */
+  /** Output full file URLs (default: true) */
   public bool $fullFileUrls = true;
 
-  /** Parse custom fields of files? */
+  /** Parse custom fields of files? (default: false) */
   public bool $parseFileCustomFields = false;
 
-  /** Key name for custom fields of files */
+  /** Key name for custom fields of files (default: '_custom_fields') */
   public string $fileCustomFieldsKey = '_custom_fields';
 }
